2670298160ab8ecd66d88ef622dc9ba51985e3d6
yuxudong <xudongyu@kugou.net>
2019-01-14 21:45:39 +0800

    调整模块目录

--- config/config.php
+++ config/config.php
@@ -68,7 +68,7 @@ return [
         |
         */
 
-        'modules' => base_path('Modules'),
+        'modules' => base_path('modules'),
         /*
         |--------------------------------------------------------------------------
         | Modules assets path
